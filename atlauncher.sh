021d29ebf37158b7bcab8f45e232e70bcd723319
Andrew Thurman <ajtbecool@gmail.com>
2021-05-16 12:35:58 -0400

    Even prettier data directory

--- atlauncher.sh
+++ atlauncher.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-mkdir ~/.var/app/io.github.ATLauncher/data/cache
-mkdir ~/.var/app/io.github.ATLauncher/data/configs
-ln -s ~/.var/app/io.github.ATLauncher/data/cache ~/.var/app/io.github.ATLauncher/cache/cache
-ln -s ~/.var/app/io.github.ATLauncher/data/configs ~/.var/app/io.github.ATLauncher/config/configs
+ln -sn ~/.var/app/io.github.ATLauncher/cache/ ~/.var/app/io.github.ATLauncher/data/cache
+ln -sn ~/.var/app/io.github.ATLauncher/config ~/.var/app/io.github.ATLauncher/data/configs
 /app/jre/bin/java -jar /app/bin/ATLauncher.jar --working-dir=.var/app/io.github.ATLauncher/data/ --no-launcher-update
