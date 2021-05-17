b64de7062d305144b690f061df5dede1e63bfc85
Andrew Thurman <ajtbecool@gmail.com>
2021-05-17 08:24:33 -0400

    rename to fit standards will switch to com.atlauncher.ATlauncher with upstream permission

--- atlauncher.sh
+++ atlauncher.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ln -sn ~/.var/app/io.github.ATLauncher/cache/ ~/.var/app/io.github.ATLauncher/data/cache
-ln -sn ~/.var/app/io.github.ATLauncher/config ~/.var/app/io.github.ATLauncher/data/configs
-/app/jre/bin/java -jar /app/bin/ATLauncher.jar --working-dir=.var/app/io.github.ATLauncher/data/ --no-launcher-update
+ln -sn ~/.var/app/io.atlauncher.ATLauncher/cache/ ~/.var/app/io.atlauncher.ATLauncher/data/cache
+ln -sn ~/.var/app/io.atlauncher.ATLauncher/config ~/.var/app/io.atlauncher.ATLauncher/data/configs
+/app/jre/bin/java -jar /app/bin/ATLauncher.jar --working-dir=.var/app/io.atlauncher.ATLauncher/data/ --no-launcher-update
