e92810e1c9cb571214d90be8d374e1cc7ce4d7c3
Andrew Thurman <ajtbecool@gmail.com>
2021-05-17 09:26:49 -0400

    fulfill upstream requirements

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# io.atlauncher.ATLauncher
+# com.atlauncher.ATLauncher
 (Flatpak) ATLauncher is a Launcher for Minecraft.
 
 ATLauncher is a Launcher for Minecraft which integrates multiple different ModPacks to allow you to download and install ModPacks easily and quickly. This package is community maintained and issues with it should not be reported to the ATLauncher upstream.
 
 Build with:
 ```sh
-rm -rf build-dir .flatpak-builder/ && flatpak-builder build-dir/ io.atlauncher.ATLauncher.yml --install --user
+rm -rf build-dir .flatpak-builder/ && flatpak-builder build-dir/ com.atlauncher.ATLauncher.yml --install --user
 ```
--- atlauncher.sh
+++ atlauncher.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ln -sn ~/.var/app/io.atlauncher.ATLauncher/cache/ ~/.var/app/io.atlauncher.ATLauncher/data/cache
-ln -sn ~/.var/app/io.atlauncher.ATLauncher/config ~/.var/app/io.atlauncher.ATLauncher/data/configs
-/app/jre/bin/java -jar /app/bin/ATLauncher.jar --working-dir=.var/app/io.atlauncher.ATLauncher/data/ --no-launcher-update
+ln -sn ~/.var/app/com.atlauncher.ATLauncher/cache/ ~/.var/app/com.atlauncher.ATLauncher/data/cache
+ln -sn ~/.var/app/com.atlauncher.ATLauncher/config ~/.var/app/com.atlauncher.ATLauncher/data/configs
+/app/jre/bin/java -jar /app/bin/ATLauncher.jar --working-dir=.var/app/com.atlauncher.ATLauncher/data/ --no-launcher-update
